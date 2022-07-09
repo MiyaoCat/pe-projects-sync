d50f9a8f523963b11445eb977eaac6c034bc491c
john <jjmiyao@gmail.com>
2022-07-09 15:55:14 -0700

    CRUD route-based styling site menu fix

--- php-crud2/site-menu.php
+++ php-crud2/site-menu.php
@@ -1,19 +1,22 @@
 
 
 <nav class="site-menu">
-	<a class="<?php if ($page =='pages/home') 
+	<a class="<?php if ($page =='home') 
 		{ echo 'active'; } ?>" 
 		href="?page=home">Home</a>
 
-	<a class="<?php if ($page =='pages/list') { echo 'active'; }?>" 
+	<a class="<?php if ($page =='list' AND $sport !='basketball' AND $sport !='football') { echo 'active'; }?>" 
 		href="?page=list">All Athletes</a>
-	<a class="<?php if ($page =='pages/basketball') { echo 'active'; }?>" 
+
+	<a class="<?php if ($sport=='basketball') { echo 'active';}?>" 
 		href="?page=list&sport=basketball">Basketball</a>
-	<a class="<?php if ($page =='pages/football') { echo 'active'; }?>" 
+
+	<a class="<?php if ($sport == 'football') { echo 'active'; }?>" 
 		href="?page=list&sport=football">Football</a>
 	<a class="<?php if ($page =='pages/huskies') { echo 'active'; }?>" 
 		href="?page=list&college=University of Washington">Huskies</a>
-	<a class="<?php if ($page =='pages/create') { echo 'active'; }?>" 
+
+	<a class="<?php if ($page =='create') { echo 'active'; }?>" 
 		href="?page=create">Add an Athlete</a>
 </nav>
 
