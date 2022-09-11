9c0b6ea57de81c0fae80ee0f1b34b9354af9fbb3
john <jjmiyao@gmail.com>
2022-09-11 15:40:55 -0700

    Completed FAQS section CSS

--- projects/figma-to-code/templates/components/accordion.php
+++ projects/figma-to-code/templates/components/accordion.php
@@ -1,14 +1,19 @@
-<?php  
-	if ($type == "accordion") {
-		foreach ($component["listItems"] as $item) {
-			$question = $item["question"];
-			$answer = $item["answer"];
-?>
-		<details>
-			<summary>
-				<h3 class="attention-voice"><?=$question?></h3>
-			</summary>
-			<p class="normal-voice"><?=$answer?></p>
-		</details>
-<?php } ?>
+<ul class="faqs-accordion">
+	<?php  
+		if ($type == "accordion") {
+			foreach ($component["listItems"] as $item) {
+				$question = $item["question"];
+				$answer = $item["answer"];
+	?>
+			<li class="faqs-card">
+				<details>
+					<summary>
+						<h3 class="question normal-voice"><?=$question?></h3>
+						<arrow><?php include("templates/icons/arrow/right-arrow.svg"); ?></arrow>
+					</summary>
+					<p class="answer calm-voice"><?=$answer?></p>
+				</details>
+			</li>
 	<?php } ?>
+		<?php } ?>
+</ul>
