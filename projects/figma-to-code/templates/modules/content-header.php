9d525c542131b61850c4727cab9eb4a539079d1c
john <jjmiyao@gmail.com>
2022-09-11 00:37:23 -0700

    Added content-header module and styled

---
+++ projects/figma-to-code/templates/modules/content-header.php
@@ -0,0 +1,13 @@
+<?php  
+	if ($type == "content-header") {
+		$header = $module["header"];
+		$subHeader = $module["subHeader"];
+
+		if ( isset($module["subHeader2"]) ) {
+			$subHeader2 = $module["subHeader2"];
+?>	
+	<p class="sub-header normal-voice"><?=$subHeader?></p>
+	<h1 class="header loud-voice"><?=$header?></h1>
+	<p class="sub-header2 normal-voice"><?=$subHeader2?></p>
+		<?php } ?>
+	<?php } ?>
--- projects/figma-to-code/templates/modules/section-header.php
+++ projects/figma-to-code/templates/modules/section-header.php
@@ -2,12 +2,10 @@
 	if ($type == "section-header") {
 		$header = $module["header"];
 		$subHeader = $module["subHeader"];
-
-		if ( isset($module["subHeader2"]) ) {
-			$subHeader2 = $module["subHeader2"];
-		}
 ?>
-	<p class="sub-header normal-voice"><?=$subHeader?></p>
+	
 	<h1 class="header loud-voice"><?=$header?></h1>
-	<p class="sub-header2 normal-voice"><?=$subHeader2?></p>
+	<p class="sub-header normal-voice"><?=$subHeader?></p>
 <?php } ?>
+
+
