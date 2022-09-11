a8aa11a1a72cd0da20e7427d27e19976ae92fe52
john <jjmiyao@gmail.com>
2022-09-11 13:15:58 -0700

    Added function to include arrows in buttons

--- projects/figma-to-code/templates/components/button.php
+++ projects/figma-to-code/templates/components/button.php
@@ -1,12 +1,33 @@
 <?php
-		$text = $component["text"];
-		$buttonType = $component["buttonType"];
-		$corners = $component["corners"];
-		$arrows = $component["arrows"];
-		$style = $component["style"];
-		$size = $component["size"];
+if ($component["arrows"] == "none") {
+	$text = $component["text"];
+	$buttonType = $component["buttonType"];
+	$corners = $component["corners"];
+	$arrows = $component["arrows"];
+	$style = $component["style"];
+	$size = $component["size"];
 ?>		
+
 	<button class="<?=$buttonType?> <?=$corners?> <?=$arrows?> <?=$style?> <?=$size?>">
 		<p class="normal-voice"><?=$text?></p>
 	</button>
 
+<?php } ?>
+
+
+<?php if ( isset($component["arrowFile"]) ) { 
+	$arrowFile = $component["arrowFile"];
+	$text = $component["text"];
+	$buttonType = $component["buttonType"];
+	$corners = $component["corners"];
+	$arrows = $component["arrows"];
+	$style = $component["style"];
+	$size = $component["size"];
+?>
+
+	 <button class="<?=$buttonType?> <?=$corners?> <?=$arrows?> <?=$style?> <?=$size?>">
+		<p class="normal-voice"><?=$text?></p>
+		<?php include("$arrowFile"); ?>
+	</button>
+
+<?php } ?>
--- projects/figma-to-code/templates/modules/section-header.php
+++ projects/figma-to-code/templates/modules/section-header.php
@@ -1,11 +1,24 @@
 <?php  
+if ($section["name"] !== "call-to-action") {
 	if ($type == "section-header") {
 		$header = $module["header"];
 		$subHeader = $module["subHeader"];
+	}
 ?>
-	
+
 	<h1 class="header loud-voice"><?=$header?></h1>
 	<p class="sub-header normal-voice"><?=$subHeader?></p>
 <?php } ?>
 
+<?php 
+	if ($section["name"] == "call-to-action") {
+		if ($type == "section-header") {
+			$header = $module["header"];
+			$subHeader = $module["subHeader"];
+		}
+?>
+
+	<p class="sub-header normal-voice"><?=$subHeader?></p>
+	<h1 class="header loud-voice"><?=$header?></h1>
+<?php } ?>
 
