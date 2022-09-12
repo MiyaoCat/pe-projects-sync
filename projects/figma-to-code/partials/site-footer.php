fc7e2a5eb097fa80b384ee3fd0b25df2ef73f190
john <jjmiyao@gmail.com>
2022-09-11 17:38:00 -0700

    Top half of footer completed

--- projects/figma-to-code/partials/site-footer.php
+++ projects/figma-to-code/partials/site-footer.php
@@ -1,33 +1,53 @@
-<footer class="site-footer">
-	<site-map>
-		<ul class="product">
-			<h4 class="normal-voice">Product</h4>
-			<li class="normal-voice">Diam orci</li>
-			<li class="normal-voice">Mi feugiat</li>
-			<li class="normal-voice">Mi feugiat</li>
-			<li class="normal-voice">Suspendisse viverra</li>
-			<li class="normal-voice">Id dolor</li>
-			<li class="normal-voice">Erat mattis</li>
-		</ul>	
-		
-		<ul class="information">
-			<h4 class="normal-voice">Information</h4>
-		
-			<li class="normal-voice">Nibh</li>
-			<li class="normal-voice">Egestas</li>
-			<li class="normal-voice">Dictum</li>
-		</ul>	
-		
-		<ul class="information">
-			<h4 class="normal-voice">Company</h4>
-		
-			<li class="normal-voice">Id maecenas</li>
-			<li class="normal-voice">Id orci</li>
-			<li class="normal-voice">Mi feugiat</li>
-			<li class="normal-voice">Magna ultricies</li>
-			<li class="normal-voice">IQuis risus</li>
-		</ul>	
-		
+<site-map>
+	<ul class="product">
+		<li><h4 class="calm-voice">Product</h4></li>
+
+		<li class="calm-voice">Diam orci</li>
+		<li class="calm-voice">Mi feugiat</li>
+		<li class="calm-voice">Mi feugiat</li>
+		<li class="calm-voice">Suspendisse viverra</li>
+		<li class="calm-voice">Id dolor</li>
+		<li class="calm-voice">Erat mattis</li>
+	</ul>	
+	
+	<ul class="information">
+		<li><h4 class="calm-voice">Information</h4></li>
+	
+		<li class="calm-voice">Nibh</li>
+		<li class="calm-voice">Egestas</li>
+		<li class="calm-voice">Dictum</li>
+	</ul>	
+	
+	<ul class="Company">
+		<li><h4 class="calm-voice">Company</h4></li>
+	
+		<li class="calm-voice">Id maecenas</li>
+		<li class="calm-voice">Id orci</li>
+		<li class="calm-voice">Mi feugiat</li>
+		<li class="calm-voice">Magna ultricies</li>
+		<li class="calm-voice">IQuis risus</li>
+	</ul>	
+	
+	<subscribe-box>
 		<?php include("templates/components/subscribe-box.php"); ?>
-	</site-map>
-</footer>
+	</subscribe-box>
+</site-map>
+
+<footer-requirements>
+	
+	<logo>
+
+	</logo>
+
+	<legal-links>
+		<a href="#">Terms</a>
+		<a href="#">Privacy</a>
+		<a href="#">Cookies</a>
+	</legal-links>
+
+	<social-links>
+		<a href="#"></a>
+		<a href="#"></a>
+		<a href="#"></a>
+	</social-links>
+</footer-requirements>
--- projects/figma-to-code/templates/components/subscribe-box.php
+++ projects/figma-to-code/templates/components/subscribe-box.php
@@ -1,15 +1,12 @@
-<subscribe-box>
 
-	<h4 class="normal-voice">Subscribe</h4>
+<h4 class="normal-voice">Subscribe</h4>
 
-	<subscribe-module>
-		<input type="email" placeholder="Email address">
-		
-		<a href="#" class="button-wrap">
-			<?php include("templates/icons/arrow/right-arrow.svg"); ?>
-		</a>
-	</subscribe-module>
+<subscribe-module>
+	<input type="email" placeholder="Email address">
 	
-	<p class="quiet-voice">Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi. </p>
+	<a href="#" class="button-wrap">
+		<?php include("templates/icons/arrow/right-arrow.svg"); ?>
+	</a>
+</subscribe-module>
 
-</subscribe-box>
+<p class="quiet-voice">Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi. </p>
