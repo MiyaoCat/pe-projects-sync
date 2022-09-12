a7b9f7691f2cf0b4beb0a180ab7076664eea6b4e
john <jjmiyao@gmail.com>
2022-09-11 20:28:09 -0700

    Styling details

--- projects/figma-to-code/templates/components/accordion.php
+++ projects/figma-to-code/templates/components/accordion.php
@@ -4,16 +4,20 @@
 			foreach ($component["listItems"] as $item) {
 				$question = $item["question"];
 				$answer = $item["answer"];
+				$open = "";
+				if ($item["open"] == true) {
+					$open = "open";
+				}
 	?>
 			<li class="faqs-card">
 				<details>
-					<summary>
+					<summary <?=$open?>>
 						<h3 class="question normal-voice"><?=$question?></h3>
 						<arrow><?php include("templates/icons/arrow/right-arrow.svg"); ?></arrow>
 					</summary>
 					<p class="answer calm-voice"><?=$answer?></p>
 				</details>
 			</li>
-	<?php } ?>
 		<?php } ?>
+	<?php } ?>
 </ul>
