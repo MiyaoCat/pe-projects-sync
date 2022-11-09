705bb54a89abb5757c0852ca028d4ad6cc65c93f
john <jjmiyao@gmail.com>
2022-11-08 22:23:17 -0800

    Added meta image

--- layout-challenges/index.php
+++ layout-challenges/index.php
@@ -8,9 +8,11 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-	<meta property="og:image" content="[IMAGE URL]">
-	<meta name="description" content="Layout gardening">
-	<title>Layout Challenges</title>
+	<link rel="icon" type="image/x-icon" href="./images/logo.svg">
+
+	<meta property="og:image" content="./images/meta-image.png">
+	<meta name="description" content="A collection of responsive modules">
+	<title>Layout Garden</title>
 
 	<link rel="stylesheet" href="css/style.css">
 </head>
