552dd49b3d60aefd4607ec3860e60613698b9e2d
john <jjmiyao@gmail.com>
2022-11-08 23:12:21 -0800

    Styled article-cards to overlap intro

--- layout-challenges/layouts/layout3.php
+++ layout-challenges/layouts/layout3.php
@@ -1,49 +1,46 @@
 
 
 <intro>
+	<teaser>
+		<p class="teaser normal-voice">Little teaser</p>
+	</teaser>
 	
-		<teaser>
-			<p class="teaser normal-voice">Little teaser</p>
-		</teaser>
-		<h2 class="attention-voice">Heading Level 2 small</h2>
+	<h2 class="attention-voice">Heading Level 2 small</h2>
 
-		<p class="intro normal-voice">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore, sequi. Dolor voluptate facilis officia nisi!</p>
-	
+	<p class="intro normal-voice">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore, sequi. Dolor voluptate facilis officia nisi!</p>
 </intro>
 
 <article>
-	
-		<ul class="article-list">
-		 	
-		 	<li class="article-card">
-		 		<icon>
-		 		<?php include("./images/circle.php"); ?>
-		 		</icon>
-		 
-		 		<h3 class="attention-voice">Heading level 3 small</h3>
+	<ul class="article-list">
+	 	
+	 	<li class="article-card">
+	 		<icon>
+	 		<?php include("./images/circle.php"); ?>
+	 		</icon>
+	 
+	 		<h3 class="attention-voice">Heading level 3 small</h3>
 
-		 		<p class="quiet-voice">Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Nobis, veniam, sit! Architecto ipsa, blanditiis fugit.</p>
-		 	</li>
+	 		<p class="quiet-voice">Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Nobis, veniam, sit! Architecto ipsa, blanditiis fugit.</p>
+	 	</li>
 
-		 	<li class="article-card">
-		 		<icon>
-		 		<?php include("./images/circle.php"); ?>
-		 		</icon>
-		 
-		 		<h3 class="attention-voice">Heading level 3 small</h3>
+	 	<li class="article-card">
+	 		<icon>
+	 		<?php include("./images/circle.php"); ?>
+	 		</icon>
+	 
+	 		<h3 class="attention-voice">Heading level 3 small</h3>
 
-		 		<p class="quiet-voice">Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Nobis, veniam, sit! Architecto ipsa, blanditiis fugit.</p>
-		 	</li>
+	 		<p class="quiet-voice">Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Nobis, veniam, sit! Architecto ipsa, blanditiis fugit.</p>
+	 	</li>
 
-		 	<li class="article-card">
-		 		<icon>
-		 		<?php include("./images/circle.php"); ?>
-		 		</icon>
-		 
-		 		<h3 class="attention-voice">Heading level 3 small</h3>
+	 	<li class="article-card">
+	 		<icon>
+	 		<?php include("./images/circle.php"); ?>
+	 		</icon>
+	 
+	 		<h3 class="attention-voice">Heading level 3 small</h3>
 
-		 		<p class="quiet-voice">Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Nobis, veniam, sit! Architecto ipsa, blanditiis fugit.</p>
-		 	</li>
-		</ul>
-	
+	 		<p class="quiet-voice">Lorem ipsum dolor sit amet consectetur, adipisicing, elit. Nobis, veniam, sit! Architecto ipsa, blanditiis fugit.</p>
+	 	</li>
+	</ul>
 </article>
