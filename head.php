dc5af4137857131c79f119824a1aed6dafa96d25
john <jjmiyao@gmail.com>
2023-01-15 11:45:35 -0800

    Removed link in meta image

--- head.php
+++ head.php
@@ -8,6 +8,6 @@
 
 	<title>Miyaomix.com 🐅 </title>
 	<meta name="description" content="Welcome to John's project site. Click or tap to view John's work!">
-	<meta property="og:image" content="https://peprojects.dev/alpha-4/john/meta-image2.jpg" />
+	<meta property="og:image" content="" />
 	<link rel="stylesheet" href="style.css">
 </head>
--- php-crud2/index.php
+++ php-crud2/index.php
@@ -55,7 +55,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>CRUD FUN</title>
-		<meta property="og:image" content="https://peprojects.dev/alpha-4/john/"/>
+		<meta property="og:image" content=""/>
 		<link rel="stylesheet" href="css/site.css">
 	</head>	
 
