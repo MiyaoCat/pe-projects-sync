b45b6c37e661b84bdc3cf26806ec749a269a813b
john <jjmiyao@gmail.com>
2023-01-15 11:38:27 -0800

    Removed link in meta image

--- crud-practice/index.php
+++ crud-practice/index.php
@@ -2,7 +2,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Oh CRUD</title>
-	<meta property="og:image" content="https://peprojects.dev/alpha-4/john/"/>
+	<meta property="og:image" content=""/>
 	<link rel="stylesheet" href="style.css">
 </head>	
 
