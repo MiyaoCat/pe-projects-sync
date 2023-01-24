a57440dd21e9c1d7dbd77df17aeef8ad061c7c91
john <jjmiyao@gmail.com>
2023-01-23 23:52:27 -0800

    First iteration of remove function

--- javascript/todo/todo.js
+++ javascript/todo/todo.js
@@ -1,72 +1,148 @@
-
 function update(note = "") {
-	console.log(`----- ${note}`);
-	console.log('todos: ', todos);
+	console.log(`|--- ${note} ------|`);
+	console.log('Groceries', groceryList);
 }
 
-const todos = [];
+const groceryList = [];
 var count = 1;
 
-function add(content) {
-	const todo = {
+function add(item) {
+	const list = {
 		id: `a${count++}`,
-		content: content
+		item: item
 	};
-	todos.push(todo);
-	update(`Added ${content}`);
+	groceryList.push(list);
+	update(`Added ${item}`);
 }
 
-function remove(id) {
-	update(`Removed ${todos[id].content}`);
-	todos.splice(id, 1);
-}
+
+function remove(id) { 
+	update(`Removed ${groceryList[id].item}`);
+	groceryList.splice(groceryList.id, 1);
+};
 
 function complete(id) {
-	todos[id].complete = true;
-	update(`Completed ${todos[id].content}`);
+	groceryList[id].complete = true;
+	update(`Retreived ${groceryList[id].item}`);
 }
 
+function updateList(id, newItem) {
+		groceryList[id].item = newItem;
+	update(`Item Updated ${groceryList[id].item}`);
+}
 
-function updateObject(searchId, newContent) {
-	// var found = false;
+add('parsley');
+add('lemon');
 
-	// todos.forEach(function(todo, i){
-	// 	if (todo.id == searchId) {
-	// 		found = todo;
-	// 	}
-	// });
-	// found.content = newContent;
+complete(0);
+
+add('ground beef');
+
+remove(0);
+
+add('tomatoes');
+
+updateList(1, 'noodles');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//WORK WITH DEREK
+// function update(note = "") {
+// 	console.log(`----- ${note}`);
+// 	console.log('todos: ', todos);
+// }
+
+// const todos = [];
+// var count = 1;
+
+// function add(content) {
+// 	const todo = {
+// 		id: `a${count++}`,
+// 		content: content
+// 	};
+// 	todos.push(todo);
+// 	update(`Added ${content}`);
+// }
+
+// function remove(id) {
+// 	update(`Removed ${todos[id].content}`);
+// 	todos.splice(id, 1);
+// }
+
+// function complete(id) {
+// 	todos[id].complete = true;
+// 	update(`Completed ${todos[id].content}`);
+// }
+
+
+// function updateObject(searchId, newContent) {
+// 	// var found = false;
+
+// 	// todos.forEach(function(todo, i){
+// 	// 	if (todo.id == searchId) {
+// 	// 		found = todo;
+// 	// 	}
+// 	// });
+// 	// found.content = newContent;
 	
-	const found = todos.find(function(todo) {
-		return todo.id == searchId;
-	});
+// 	const found = todos.find(function(todo) {
+// 		return todo.id == searchId;
+// 	});
 
-	if (found) {
-		found.content = newContent;
-		update(`Updated ${found.content}`);
+// 	if (found) {
+// 		found.content = newContent;
+// 		update(`Updated ${found.content}`);
 
-	} else {
-		console.log('searchId not found');
-	}
+// 	} else {
+// 		console.log('searchId not found');
+// 	}
 
-	console.log(found);
+// 	console.log(found);
 
-	// update(`Updated ${found.content}`);
-}
+// 	// update(`Updated ${found.content}`);
+// }
 
-add("Make breafast")
-add("drink water");
-add("stretch");
-add("exercise");
+// add("Make breafast")
+// add("drink water");
+// add("stretch");
+// add("exercise");
 
-complete(2);
-add("read");
+// complete(2);
+// add("read");
 
-remove(2);
+// remove(2);
 
-add("Go to sleep!");
+// add("Go to sleep!");
 
-updateObject('a2', 'Eat food');
+// updateObject('a2', 'Eat food');
 
-remove(1);
-remove(2);
+// remove(1);
+// remove(2);
