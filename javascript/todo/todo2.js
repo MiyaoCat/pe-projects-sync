8bc65ce633772861a712800a8d9918a547f9953a
john <jjmiyao@gmail.com>
2023-01-26 18:09:25 -0800

    Encapsulated list

--- javascript/todo/todo2.js
+++ javascript/todo/todo2.js
@@ -1,89 +1,142 @@
-var goals = [];
-var count = 1;
+//LIST WITH ENCAPSULATION (FUNCTIONS AS OBJECTS)
+const morningRoutine = {
 
-function log(note = "") {
-	console.log(`----${note}`);
-	console.log('Goals: ', goals);
-}
+	routine: [],
+	customId: 1,
 
-function findById(searchId) {
-	return goals.find( function(specificGoal) {
-		return specificGoal.id === searchId;
-	});
-}
+	log: function(note = "") {
+		console.log(`----${note}`);
+		console.log('Routine: ', this.routine);
+	},
 
-function add(newItem) {
-	const list = {
-		id: `a${count++}`,
-		goal: newItem,
-	};
-	goals.push(list);
+	findCustomId: function(searchId) {
+		return this.routine.find( function(routineStep) {
+			return routineStep.customId === this.searchId;
+		});
+	},
 
-	log(`Added: ${newItem}`);
-};
-
-function remove(customID) {
-	const found = findById(customID); 
-		if (found) {
-			goals.splice(customID, 1);
-			log(`Removed: ${found.goal}`);
-		} else {
-			console.log(customID + ' not found');
+	add: function(step) {
+		const routineObject = {
+			id: `a${this.customId++}`,
+			action: step,
 		}
+		this.routine.push(routineObject);
+		this.log(`Added: ${step}`);
+	},
 
-	// log(`Removed: ${goals[id].goal}`)
-	// goals.splice(id, 1);
+	remove: function(searchId) {
+		let found = this.findCustomId(searchId);
+		if (found) {
+			this.routine.splice(searchId, 1);
+			this.log(`Removed: ${found.action}`);
+		} else {
+			console.log(searchId + ' not found');
+		}
+	},
+
+	complete: function(searchId) {
+		let found = this.findCustomId(searchId);
+		if (found) {
+			this.log(`Completed: ${found.action}`);
+		} else {
+			console.log(searchId + ' not found');
+		}
+	},
+
+	update: function(searchId, newAction) {
+		let found = this.findCustomId(searchId);
+		if (found) {
+			this.log(`Update: ${found.action} has been changed to ${newAction}`);
+		} else {
+			console.log(searchId + ' not found');
+		}
+	}
 }
 
-function complete(searchID) {
-	const found = findById(searchID);
+morningRoutine.add('Make coffee');
+morningRoutine.add('Brush teeth');
+morningRoutine.add('stetch');
+morningRoutine.add('Read email');
 
-	if (found) {
-		found.complete = true;
-		log(`Completed: ${found.goal}`)
-	} else {
-		console.log(searchID + ' not found');
-	}
+morningRoutine.complete('a1');
 
-	// goals[searchID].complete = true;
-	// log(`Completed: ${goals[searchID].goal}`)
-}
+morningRoutine.remove('a1');
 
-function update(searchID, newGoal) {
-	const found = findById(searchID);
+morningRoutine.update('a4', 'Water plants');
 
-	if (found) {
-		log(`Updated goal: ${found.goal} changed to ${newGoal}`);
-		found.goal = newGoal;
-	} else {
-		console.log(searchID + ' not found');
-	}
+//LIST WITH NO INTERFACE
+// var goals = [];
+// var count = 1;
 
-	// log(`Updated goal: ${goals[id].goal} changed to ${newGoal}`)
-	// goals[id].goal = newGoal;
+// function log(note = "") {
+// 	console.log(`----${note}`);
+// 	console.log('Goals: ', goals);
+// }
+
+// function findById(searchId) {
+// 	return goals.find( function(specificGoal) {
+// 		return specificGoal.id === searchId;
+// 	});
+// }
+
+// function add(newItem) {
+// 	const list = {
+// 		id: `a${count++}`,
+// 		goal: newItem,
+// 	};
+// 	goals.push(list);
+
+// 	log(`Added: ${newItem}`);
+// };
+
+// function remove(customID) {
+// 	const found = findById(customID); 
+// 		if (found) {
+// 			goals.splice(customID, 1);
+// 			log(`Removed: ${found.goal}`);
+// 		} else {
+// 			console.log(customID + ' not found');
+// 		}
+
+// 	// log(`Removed: ${goals[id].goal}`)
+// 	// goals.splice(id, 1);
+// }
+
+// function complete(searchID) {
+// 	const found = findById(searchID);
+
+// 	if (found) {
+// 		found.complete = true;
+// 		log(`Completed: ${found.goal}`)
+// 	} else {
+// 		console.log(searchID + ' not found');
+// 	}
+
+// 	// goals[searchID].complete = true;
+// 	// log(`Completed: ${goals[searchID].goal}`)
+// }
+
+// function update(searchID, newGoal) {
+// 	const found = findById(searchID);
+
+// 	if (found) {
+// 		log(`Updated goal: ${found.goal} changed to ${newGoal}`);
+// 		found.goal = newGoal;
+// 	} else {
+// 		console.log(searchID + ' not found');
+// 	}
+
+// 	// log(`Updated goal: ${goals[id].goal} changed to ${newGoal}`)
+// 	// goals[id].goal = newGoal;
 	
-}
+// }
 
-add('Get a Front End Dev Job');
-add('Cycle 200 miles in a month');
-add('Create a JS list without help!');
-complete('a1');
-remove('a12');
-update('a2', 'Complete a Century Ride');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// add('Get a Front End Dev Job');
+// add('Cycle 200 miles in a month');
+// add('Create a JS list without help!');
+// complete('a1');
+// remove('a1');
+// update('a2', 'Complete a Century Ride');
 
 
 
