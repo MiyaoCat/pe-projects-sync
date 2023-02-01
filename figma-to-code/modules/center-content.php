e7f33c79e6bfbde10f675d45c959e2f331dfe5b1
john <jjmiyao@gmail.com>
2023-01-31 23:15:33 -0800

    Styled center-content

--- figma-to-code/modules/center-content.php
+++ figma-to-code/modules/center-content.php
@@ -1,16 +1,15 @@
 <?php if($type == "center-content") { ?>
+	<?php if ( isset($module['sub-header2']) ) { ?>
+		<p class="alert-voice"><?=$module['sub-header2']?></p>
+	<?php } ?>
 
 	<h2 class="sub-header attention-voice"><?=$header?></h2>
-	<p class="info normal-voice"><?=$subHeader?></p>
+	<p class="info alert-voice"><?=$subHeader?></p>
 
 	<div class="video">
 		<?php foreach($components as $component) {
 			include("components/$component[type].php");
 		} ?>
 	</div>
-
-	<?php if ( isset($module['sub-header2']) ) { ?>
-		<p class="normal-voice"><?=$module['sub-header2']?></p>
-	<?php } ?>
 
 <?php } ?>
