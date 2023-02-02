cce9448e93ca964c69cd55315e72e48c22f7dc7f
john <jjmiyao@gmail.com>
2023-02-01 19:07:44 -0800

    Rendered company list

--- figma-to-code/modules/site-footer.php
+++ figma-to-code/modules/site-footer.php
@@ -3,29 +3,32 @@
 	$footerData = json_decode($json, true);
 ?>
 
-<?php  
-	foreach ($footerData as $component) {
+<?php  foreach ($footerData as $component) { ?>
+	
+	<?php 
 		if($component["type"] == "product-list") { 
-			$heading = $component["heading"];
-?>
-		<div class="site-map">
-			<ul class=<?=$heading?>>
-				<h4 class="alert-voice"><?=$heading?></h4>
-				<?php  
-					foreach($component["items"] as $item) {
-						$text = $item['text'];
-				?>
-				<li>
-					<p class="alert-voice"><?=$text;?></p>
-				</li>
-				<?php } ?>
-			</ul>
-				<?php } ?>
+			$heading = $component["heading"]; ?>
+			
+			<div class="site-map">
+				<ul class=<?=$heading?>>
+					<h4 class="alert-voice"><?=$heading?></h4>
+					<?php  
+						foreach($component["items"] as $item) {
+							$text = $item['text'];
+					?>
+						<li>
+							<p class="alert-voice"><?=$text;?></p>
+						</li>
+					<?php } ?>
+				</ul>
+
+		<?php } ?>
 			
 	<?php  
 		if($component["type"] == "information-list") { 
-		$heading = $component["heading"];
+			$heading = $component["heading"];
 	?>
+
 			<ul class=<?=$heading?>>
 				<h4 class="alert-voice"><?=$heading?></h4>
 		
@@ -37,9 +40,33 @@
 						<p class="alert-voice"><?=$text;?></p>
 					</li>
 				<?php } ?>
+
 			</ul>
 		</div>
 
 	<?php } ?>
+
+	<?php  
+		if($component["type"] == "company-list") { 
+			$heading = $component["heading"];
+	?>
+
+			<ul class=<?=$heading?>>
+				<h4 class="alert-voice"><?=$heading?></h4>
+		
+				<?php  
+					foreach($component["items"] as $item) {
+						$text = $item['text'];
+				?>
+					<li>
+						<p class="alert-voice"><?=$text;?></p>
+					</li>
+				<?php } ?>
+				
+			</ul>
+		</div>
+
+	<?php } ?>
+
 <?php } ?>
 
