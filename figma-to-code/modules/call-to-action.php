eb0164abbbc8e41fbd63e391afd70c6a2087029c
john <jjmiyao@gmail.com>
2023-02-03 01:58:11 -0800

    Styled call-to-action module

--- figma-to-code/modules/call-to-action.php
+++ figma-to-code/modules/call-to-action.php
@@ -3,12 +3,14 @@
 	<p class="teaser alert-voice"><?=$subHeader?></p>
 	<h2 class="header attention-voice"><?=$header?></h2>
 
-	<?php  
-		foreach($components as $component) {
-			if($component["type"] == "button") {
-				include("components/$component[type].php");
+	<div class="buttons">
+		<?php  
+			foreach($components as $component) {
+				if($component["type"] == "button") {
+					include("components/$component[type].php");
+				}
 			}
-		}
-	?>
+		?>
+	</div>
 
 <?php } ?>
