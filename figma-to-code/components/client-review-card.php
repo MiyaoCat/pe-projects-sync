906a418e44b15fe78ac7f8d2f83e0d3cbc5e4808
john <jjmiyao@gmail.com>
2023-02-02 19:02:39 -0800

    Added content data for modules

--- figma-to-code/components/client-review-card.php
+++ figma-to-code/components/client-review-card.php
@@ -3,14 +3,17 @@
 			$rating = $component['rating'];
 			$review = $component['review'];
 			$avatar = $component['avatar'];
-			$name = $component['name'];	
+			$name = $component['name'];
 ?>
 
 	<review-card>
 		
 		<star-rating>
-			<?php foreach( [1, 2, 3, 4, 5] as $stars) {
-				include($rating); 
+			<?php 
+				if ($rating) {
+				foreach( [1, 2, 3, 4, 5] as $stars) {
+					include($rating); 
+				}
 			} ?>
 		</star-rating>
 		<p class="normal-voice"><?=$review?></p>
@@ -18,6 +21,9 @@
 		<client-details>
 			<img src="<?=$avatar?>" alt="picture of client">
 			<p class="calm-voice"><?=$name?></p>
+			<?php if ( isset($component['role']) ) { ?>
+			<p class="calmvoice"><?=$component['role']?></p>	
+			<?php	} ?>
 		</client-details>
 
 	</review-card>
