622f9737ad37d837aa914da4d96389483914a8b0
john <jjmiyao@gmail.com>
2023-02-04 02:57:49 -0800

    Added cta data and rendered

--- figma-to-code/modules/call-to-action.php
+++ figma-to-code/modules/call-to-action.php
@@ -3,14 +3,33 @@
 	<p class="teaser alert-voice"><?=$subHeader?></p>
 	<h2 class="header attention-voice"><?=ucwords($header)?></h2>
 
-	<div class="buttons">
+	
 		<?php  
 			foreach($components as $component) {
 				if($component["type"] == "button") {
+		?>
+				<div class="buttons">	
+					<?php include("components/$component[type].php"); ?>
+				</div>
+				<?php } ?>
+			<?php } ?>
+	
+	
+
+		<?php  
+			foreach($components as $component) {
+				if($component["type"] == "email-signup") {
 					include("components/$component[type].php");
 				}
 			}
 		?>
-	</div>
+
+		<?php  
+			foreach($components as $component) {
+				if($component["type"] == "image-placeholder") {
+					include("components/$component[type].php");
+				}
+			}
+		?>
 
 <?php } ?>
