71a0bc4e97b01d865d8399c9f7ce0dbbdd59eb6f
john <jjmiyao@gmail.com>
2023-02-04 09:58:26 -0800

    Styled call-to-action

--- figma-to-code/modules/call-to-action.php
+++ figma-to-code/modules/call-to-action.php
@@ -1,30 +1,24 @@
 <?php if($type == "call-to-action") { ?>
 
-	<p class="teaser alert-voice"><?=$subHeader?></p>
-	<h2 class="header attention-voice"><?=ucwords($header)?></h2>
-
-	<div class="buttons">
-		<?php  
-			foreach($components as $component) {
-				if($component["type"] == "button") {
-					include("components/$component[type].php");
-				}
-				if($component["type"] == "email-signup") {
-					include("components/$component[type].php");
-				}
-			} 
-
-
-		?>
+	<div class="content-wrap">
+		<p class="teaser alert-voice"><?=$subHeader?></p>
+		<h2 class="header attention-voice"><?=ucwords($header)?></h2>
+		
+		<div class="buttons">
+			<?php  
+				foreach($components as $component) {
+					if($component["type"] == "button") {
+						include("components/$component[type].php");
+					}
+					if($component["type"] == "email-signup") {
+						include("components/$component[type].php");
+					}
+				} 
+		
+		
+			?>
+		</div>
 	</div>
-
-		<?php  
-			// foreach($components as $component) {
-			// 	if($component["type"] == "email-signup") {
-			// 		include("components/$component[type].php");
-			// 	}
-			// }
-		?>
 
 		<?php  
 			foreach($components as $component) {
@@ -33,4 +27,5 @@
 				}
 			}
 		?>
+
 <?php } ?>
