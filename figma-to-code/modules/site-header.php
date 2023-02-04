3cef38f01d63847831048b069abafa78feddb432
john <jjmiyao@gmail.com>
2023-02-03 16:23:32 -0800

    Added page routing to make header dynamic

--- figma-to-code/modules/site-header.php
+++ figma-to-code/modules/site-header.php
@@ -1,5 +1,18 @@
 <?php 
-	$json = file_get_contents("data/site-header.json");
+
+	$pageDataFilePath = "data/pages/$page.json";
+	$pageData = null;
+
+	if ( file_exists($pageDataFilePath) ) {
+		$jsonData = file_get_contents($pageDataFilePath);
+		$pageData = json_decode($jsonData, true);
+	}
+
+	if ($pageData) {
+		$siteHeader = $pageData["site-header"];
+	}
+
+	$json = file_get_contents($siteHeader);
 	$headerComponents = json_decode($json, true);
 ?>
 	
