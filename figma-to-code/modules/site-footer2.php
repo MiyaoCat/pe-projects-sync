21bc04561d7f2ac928f51f998daf06ba4d351efe
john <jjmiyao@gmail.com>
2023-02-04 16:20:27 -0800

    Added files and styling for template-2 footer

---
+++ figma-to-code/modules/site-footer2.php
@@ -0,0 +1,97 @@
+<?php  
+	$json = file_get_contents("data/site-footer2.json");
+	$footerData = json_decode($json, true);
+?>
+
+<div class="footer-top">
+	
+	<div class="site-map">
+		<?php  
+			foreach ($footerData as $component) { 
+				if($component["type"] == "site-map") { 
+					$heading = $component["heading"]; ?>
+						
+						<ul class=<?=$heading?>>
+							<h4 class="calm-voice-bold">
+								<?=ucfirst($heading)?>			
+							</h4>
+
+							<?php  
+								foreach($component["items"] as $item) {
+									$text = $item['text'];
+							?>
+								<li>
+									<p class="quiet-voice"><?=$text;?></p>
+								</li>
+							<?php } ?>
+						</ul>
+		
+				<?php } ?>
+		<?php } ?>
+
+		<!-- SUBSCRIBE-BOX -->
+		<?php  
+			foreach ($footerData as $component) { 
+				if($component["type"] == "subscribe-box") { 
+					include("components/$component[type].php");
+				}	
+			} 
+		?>
+	</div>
+
+</div>
+
+<div class="footer-bottom">
+
+	<div class="logo">
+		<?php  
+			foreach ($footerData as $component) { 
+				if($component["type"] == "logo") { 
+					include($component["source"]);
+				}
+			}	
+		?>
+	</div>
+
+	<ul class="legal">
+		<?php  
+			foreach ($footerData as $component) { 
+				if($component["type"] == "legal") { 
+					foreach($component["items"] as $item) { 
+						$text = $item['text'];
+		?>
+				<li>
+					<p class="calm-voice"><?=$text?></p>
+				</li>
+					<?php } ?>
+				<?php } ?>
+			<?php } ?>			
+	</ul>
+
+	<ul class="socials">
+		<?php	foreach($component["items"] as $social) { ?>
+			<li>
+				<?php include($social['source']) ?>
+			</li>
+		<?php } ?>
+	</ul>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- figma-to-code/partials/footer.php
+++ figma-to-code/partials/footer.php
@@ -1,7 +1,26 @@
+
+<?php  
+	$page = "home";
+		if ( isset($_GET["page"]) ) {
+			$page = $_GET["page"];
+		}
+?>
+
+
 <footer class="site-footer">
 	<inner-column>
 		<site-footer> 
-			<?php include("modules/site-footer.php"); ?> 
+
+			<?php 
+				if ($page == "template-1" or $page == "template-3") {
+					include("modules/site-footer.php"); 
+				}
+
+				if ($page == "template-2") {
+					include("modules/site-footer2.php"); 
+				}
+			?>
+
 		</site-footer>
 	</inner-column>
 </footer>
