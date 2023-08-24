3f8cc008f4eee95c654caa8350bae6bdf7008e2a
John Miyao <jjmiyao@gmail.com>
2023-08-23 22:30:30 -0700

    Practice importing module

--- node-test/index.js
+++ node-test/index.js
@@ -1,5 +1,34 @@
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
-console.log("unique id: ", uuidv4() );
+// console.log("unique id: ", uuidv4() );
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
 
-console.log("Hi john");
+// console.log("Hi john");
+
+//ES6 EXAMPLE TO GET DIRNAME/FILENAME 1
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+ 
+//ES6 EXAMPLE TO GET DIRNAME/FILENAME 2
+// import * as url from 'url';
+// const __filename = url.fileURLToPath(import.meta.url);
+// const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+// console.log("dir name: ", __dirname);
+// console.log("file name: ", __filename);
+
+// const math = require('./math.js');
+// console.log("double: ", math.doubleUp(3));
+
+// console.log("triple: ", math.triple(3));
+
+// console.log("half: ", math.half(3));
+
+
+const { doubleUp, triple, half } = require('./math.js');
+console.log("double: ", doubleUp(3));
+
+console.log("triple: ", triple(3));
+
+console.log("half: ", half(3));
---
+++ node-test/math.js
@@ -0,0 +1,17 @@
+function double(number) {
+	return number * 2;
+}
+
+function triple(number) {
+	return number * 3;
+}
+
+function half(number) {
+	return number / 2;
+}
+
+module.exports = {
+	doubleUp: double,
+	triple,
+	half
+}
