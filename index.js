3d830e62d94d0c3b43b07039b377407f0c2d2a44
John Miyao <jjmiyao@gmail.com>
2023-08-25 16:45:36 -0700

    Add views and css

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import express from 'express';
+import monsterData from './monsters.json' assert { type: 'json' };
 import { URL } from 'node:url';
+const __dirname = new URL('.', import.meta.url).pathname;
 
 const app = express();
 const PORT = 2023;
+
+app.set('view engine', 'ejs');
+app.use( express.static('public') );
+app.use( express.static('styles') );
+
 app.get('/', function(request, response) {
-	response.send("<h1>Home Page</h1>")
+	response.render('home')
 });
 
+app.get('/monsters', function(request, response) {
+	response.render('monsters', { monsters: monsterData })
+});
+
+app.get('/monster', function(request, response) {
+	response.render("detail")
+});
+
+app.use( function(request, response) {
+	response.status(404).render('404', { query: request.url });
+})
 app.listen(PORT, () => {
 	console.log(`Server started at http://localhost:${PORT}`)
 })
